7b2523117e68815ff11cac4d631fb618f7450bb6
Seth McGuinness <seth.mcguinness@yahoo.com>
2019-02-07 15:24:01 -0600

    some testing

--- __test__/integration/account.test.js
+++ __test__/integration/account.test.js
@@ -6,64 +6,80 @@ let browser;
 const width = 1440;
 const height = 767;
 
-const APP = 'http://localhost:3000';
-
-describe('Login', () => {
+describe('Google', () => {
   beforeAll(async () => {
-    browser = await puppeteer.launch({
-      headless: true,
-      slowMo: 80,
-      args: [`--window-size=${width},${height}`],
-    });
-    page = await browser.newPage();
-    await page.deleteCookie({ name: 'MEETUP_TRACK', domain: '.meetup.com' });
-    await page.deleteCookie({ token: 'token' });
-    await page.setViewport({ width, height });
+    await page.goto('https://google.com');
   });
 
-  afterAll(async () => {
-    await page.deleteCookie({ name: 'MEETUP_TRACK', domain: '.meetup.com' });
-    await page.deleteCookie({ name: 'token' });
-    browser.close();
+  it('should display "google" text on page', async () => {
+    await expect(page).toMatch('google');
   });
-  it('should redirect to login page', async () => {
-    await page.goto(APP);
-    const pathName = await page.evaluate(() => window.location.pathname);
-    expect(pathName).toEqual('/login');
-  }, 16000);
-  it('should redirect to meetup.com to login', async () => {
-    await page.goto(`${APP}/login`);
-
-    await page.waitForSelector('button[type=button]');
-    await page.click('button[type=button]');
-    const path = await page.evaluate(() => {
-      const { origin, pathname } = window.location;
-      return { origin, pathname };
-    });
-    expect(`${path.origin}${path.pathname}`).toEqual('https://secure.meetup.com/login/');
-  }, 16000);
-  it('should authenticate meetup user', async () => {
-    await page.goto(`${APP}/login`);
-
-    await page.waitForSelector('button[type=button]');
-    await page.click('button[type=button]');
-
-    await page.waitForSelector('div[id=paneLogin]');
-    await page.click('input[name=email]');
-    await page.type('input[name=email]', 'aubobq7@wbdev.tech');
-    await page.click('input[name=password]');
-    await page.type('input[name=password]', '123456');
-    await page.click('input[name=submitButton]');
-    await page.waitFor(8000);
-    const path = await page.evaluate(() => {
-      const { origin, pathname } = window.location;
-      return { origin, pathname };
-    });
-    expect(`${path.origin}${path.pathname}`).toEqual('http://localhost:3000/');
-  }, 30000);
 });
 
-describe('Logout', () => {
+// describe('Login', () => {
+//   beforeEach(async () => {
+//     jest.
+//     await page.goto('http://localhost:3000');
+//     await page.
+//   }, 10000);
+//   it('should just work', async () => {
+//     const pathName = await page.evaluate(() => window.location.pathname);
+//     expect(pathName).toEqual('/login');
+//   });
+//   // beforeAll(async () => {
+//   //   browser = await puppeteer.launch({
+//   //     headless: true,
+//   //     slowMo: 80,
+//   //     args: [`--window-size=${width},${height}`],
+//   //   });
+//   //   page = await browser.newPage();
+//   //   await page.deleteCookie({ name: 'MEETUP_TRACK', domain: '.meetup.com' });
+//   //   await page.deleteCookie({ name: 'token', domain: 'localhost' });
+//   //   await page.setViewport({ width, height });
+//   // });
+//   //
+//   // afterAll(async () => {
+//   //   await page.deleteCookie({ name: 'MEETUP_TRACK', domain: '.meetup.com' });
+//   //   await page.deleteCookie({ name: 'token', domain: 'localhost' });
+//   //   browser.close();
+//   // });
+//   /*it('should redirect to login page', async () => {
+//     const pathName = await page.evaluate(() => window.location.pathname);
+//     expect(pathName).toEqual('/login');
+//   }, 16000);
+//   it('should redirect to meetup.com to login', async () => {
+//     await page.goto(`${APP}/login`);
+//
+//     await page.waitForSelector('button[type=button]');
+//     await page.click('button[type=button]');
+//     const path = await page.evaluate(() => {
+//       const { origin, pathname } = window.location;
+//       return { origin, pathname };
+//     });
+//     expect(`${path.origin}${path.pathname}`).toEqual('https://secure.meetup.com/login/');
+//   }, 16000);
+//   it('should authenticate meetup user', async () => {
+//     await page.goto(`${APP}/login`);
+//
+//     await page.waitForSelector('button[type=button]');
+//     await page.click('button[type=button]');
+//
+//     await page.waitForSelector('div[id=paneLogin]');
+//     await page.click('input[name=email]');
+//     await page.type('input[name=email]', 'aubobq7@wbdev.tech');
+//     await page.click('input[name=password]');
+//     await page.type('input[name=password]', '123456');
+//     await page.click('input[name=submitButton]');
+//     await page.waitFor(8000);
+//     const path = await page.evaluate(() => {
+//       const { origin, pathname } = window.location;
+//       return { origin, pathname };
+//     });
+//     expect(`${path.origin}${path.pathname}`).toEqual('http://localhost:3000/');
+//   }, 30000);*/
+// });
+
+/*describe('Logout', () => {
   beforeAll(async () => {
     browser = await puppeteer.launch({
       headless: false,
@@ -72,13 +88,13 @@ describe('Logout', () => {
     });
     page = await browser.newPage();
     await page.deleteCookie({ name: 'MEETUP_TRACK', domain: '.meetup.com' });
-    await page.deleteCookie({ name: 'token' });
+    await page.deleteCookie({ name: 'token', domain: 'localhost' });
     await page.setViewport({ width, height });
   });
 
   afterAll(async () => {
     await page.deleteCookie({ name: 'MEETUP_TRACK', domain: '.meetup.com' });
-    await page.deleteCookie({ name: 'token' });
+    await page.deleteCookie({ name: 'token', domain: 'localhost' });
     browser.close();
   });
   it('should log out a user', async () => {
@@ -103,4 +119,4 @@ describe('Logout', () => {
     });
     expect(`${path.origin}${path.pathname}`).toEqual('http://localhost:3000/login');
   }, 30000);
-});
+});*/
---
+++ jest-puppeteer.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  server: {
+    command: 'npm run start',
+    port: 3000,
+  },
+  launch: {
+    headless: false,
+    devtools: true,
+    timeout: 30000,
+  },
+};
