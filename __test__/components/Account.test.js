14fa330d3e6767d89b18c5ede698b9017157468a
Seth McGuinness <seth.mcguinness@yahoo.com>
2019-02-08 00:32:32 -0600

    finally getting tests working

--- __test__/components/Account.test.js
+++ __test__/components/Account.test.js
@@ -1,32 +1,55 @@
-import Adapter from 'enzyme-adapter-react-16';
-import Enzyme, { shallow, mount } from 'enzyme';
 import React from 'react';
-import renderer from 'react-test-renderer';
-
-import { Chip, Button } from '@material-ui/core';
-
+import Enzyme, { shallow } from 'enzyme';
 import faker from 'faker';
 import sinon from 'sinon';
+
+import Adapter from 'enzyme-adapter-react-16';
+import { createSerializer } from 'enzyme-to-json';
 
 import LoginButton from '../../components/Account/LoginButton';
 import LoggedInWidget from '../../components/Account/LoggedInWidget';
 
 Enzyme.configure({ adapter: new Adapter() });
+expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }));
 
-/*describe('Login Button', () => {
-  it('Show a login button', () => {
-    const Button = shallow(<LoginButton />);
-    console.log(Button);
+describe('Login Button', () => {
+  let wrapper;
+  beforeAll(() => {
+    wrapper = shallow(<LoginButton />);
+  });
+  it('renders correctly', () => {
+    expect(wrapper).toMatchSnapshot();
   });
 
   it('simulates click events', () => {
-    // const onButtonClick = sinon.spy();
-    const wrapper = shallow(<LoginButton />);
-    wrapper.find('Button').simulate('click');
-    expect(onButtonClick).to.have.property('callCount', 1);
+    const onButtonClick = sinon.spy();
+    wrapper.setProps({ onClick: onButtonClick });
+    wrapper.simulate('click');
+    expect(onButtonClick.callCount).toEqual(1);
   });
 });
 
+describe('LoggedInWidget', () => {
+  let wrapper;
+  beforeAll(() => {
+    const user = {
+      name: 'Keyshawn Larson',
+      photo: {
+        thumb_link: 'https://s3.amazonaws.com/uifaces/faces/twitter/bereto/128.jpg',
+      },
+    };
+    const handleLogout = sinon.spy();
+    wrapper = shallow(<LoggedInWidget handleLogout={handleLogout} user={user} />);
+  });
+  it('renders correctly', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('renders the users name', () => {
+    expect(wrapper).toBeTruthy();
+  });
+});
+/*
 describe('LoggedInWidget', () => {
   let user;
 
